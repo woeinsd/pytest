ac0172b11a0e6576cbc73f9ca21a03f8d9ef642b
woeinsd <94600863+woeinsd@users.noreply.github.com>
2021-11-22 10:47:31 +0800

    变一下

--- getactions.py
+++ getactions.py
@@ -1,5 +1,5 @@
 import  os
-access_id = os.environ['MY_GITHUB_ID']
-access_pw = os.environ['MY_GITHUB_PW']
+access_id = os.environ['ID']
+access_pw = os.environ['PW']
 print("显示：",access_id,access_pw)
 print("显示2：",1,2)
